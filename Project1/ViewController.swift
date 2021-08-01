629ff2e2bd9cc1c3f235726070de678c3839a53f
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-01 12:54:18 +0300

    import image view

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
     
     override func viewDidLoad() {
@@ -19,8 +19,6 @@ class ViewController: UIViewController {
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
-
-
             }
         }
         
@@ -31,6 +29,18 @@ class ViewController: UIViewController {
     }
 
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for:indexPath )
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
 
 }
 
