88fec1d38c42d14032fbe5d6326c6397abbe47e8
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-01 18:39:41 +0300

    Remembers how many times each storm image was shown

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
-    
-    
+    var numberOfClicks = [Int]()
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,11 +27,9 @@ class ViewController: UITableViewController {
         }
         pictures.sort()
         
-        
-        
-       
         navigationController?.navigationBar.prefersLargeTitles = true
-
+        numberOfClicks = Array(repeating: 0, count: pictures.count)
+        
     }
 
 
@@ -44,18 +42,24 @@ class ViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for:indexPath )
         cell.textLabel?.text = pictures[indexPath.row]
+        let loadedArray = defaults.object(forKey: "clicks") as? [Int] ?? [Int]()
+        cell.detailTextLabel?.text = "\(loadedArray[indexPath.row])"
         return cell
     }
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        // Incrementing frequency array & updating tableView
+        numberOfClicks[indexPath.row] += 1
+        defaults.set(numberOfClicks, forKey: "clicks")
+        tableView.reloadData()
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
             
             vc.selectedImage = pictures[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
             vc.selectedIndex = indexPath.row + 1
-            vc.numOfImages = pictures.count 
+            vc.numOfImages = pictures.count
+        
         }
     }
     
